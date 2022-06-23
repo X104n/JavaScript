9a7b30a8e1b54415aae103608bdf46ac6704e441
Stian Munkejord <stianmunkejord@gmail.com>
2022-06-24 00:10:51 +0200

    Day 2 not yet completed

---
+++ Day2/main.js
@@ -0,0 +1,8 @@
+let challange = "30 Days Of JavaScript"
+console.log(challange)
+console.log(challange.length)
+console.log(challange.toUpperCase())
+console.log(challange.toLowerCase())
+console.log(challange.substr(3))
+console.log(challange.substring(0, 3))
+console.log(challange.includes("Script"))
