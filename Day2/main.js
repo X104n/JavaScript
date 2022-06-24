4fea169e1f4e30bf03fd9008a188fa41284dff5c
X104n <stianmunkejord@gmail.com>
2022-06-24 17:47:20 +0200

    Completed level 2 of the day 2 exercises

--- Day2/main.js
+++ Day2/main.js
@@ -1,3 +1,4 @@
+// Level 1
 let challange = "30 Days Of JavaScript"
 console.log(challange)
 console.log(challange.length)
@@ -26,3 +27,27 @@ let string1 = '30 Days of'
 let string2 = 'JavaScript'
 console.log(string1.concat(string2))
 console.log(challange.repeat(2))
+
+// Level 2
+console.log("The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.")
+console.log('"Love is not patronizing and charity isn\'t about pity, it is about love. Charity and love are the same -- with charity you give love, so don\'t just give money but reach out your hand instead."')
+console.log('10' === 10)
+console.log(parseFloat('9.8') == 10)
+let search = 'on'
+let py = "python"
+let ja = "jargon"
+// If the value is above -1 it exist
+console.log(py.search(search))
+console.log(ja.search(search))
+let sentence6 = "I hope this course is not full of jargon."
+console.log(sentence6.search("jargon"))
+console.log(Math.floor(Math.random() * 101))
+console.log(Math.floor(Math.random() * (101 - 51) + 51))
+console.log(Math.floor(Math.random() * 256))
+let tString = 'JavaScript'
+console.log(tString.charAt(Math.floor(Math.random() * tString.length)))
+console.log("Just use a lot of space and the \\ character to cut to a new line")
+let finalLv2Sentence = 'You cannot end a sentence with because because because is a conjunction'
+let phrase = 'because because because'
+console.log("This final excercise makes no sence, how am i supposed to use the *substr* to cut out a part")
+console.log(finalLv2Sentence.substr(finalLv2Sentence.indexOf(phrase), finalLv2Sentence.indexOf(phrase) + phrase.length))
