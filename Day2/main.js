9c5b8ea43a8e2508c3cefcbb51f2ecd6cea34f16
X104n <stianmunkejord@gmail.com>
2022-06-24 15:31:47 +0200

    Finished level 1 Exercise of day 2

--- Day2/main.js
+++ Day2/main.js
@@ -6,3 +6,23 @@ console.log(challange.toLowerCase())
 console.log(challange.substr(3))
 console.log(challange.substring(0, 3))
 console.log(challange.includes("Script"))
+console.log(challange.split(''))
+console.log(challange.split(' '))
+let companies = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+console.log(companies.split(', '))
+console.log(challange.replace("JavaScript", "Python"))
+console.log(challange.charAt(15))
+console.log(challange.charCodeAt("J"))
+console.log(challange.indexOf("a"))
+console.log(challange.lastIndexOf("a"))
+let sentence = 'You cannot end a sentence with because because because is a conjunction'
+console.log(sentence.indexOf("because"))
+let trailing = "   This string has a lot of trailing whitespace              "
+console.log(trailing.trim())
+console.log(challange.startsWith("30"))
+console.log(challange.endsWith("Script"))
+console.log(challange.match(/a/gi))
+let string1 = '30 Days of'
+let string2 = 'JavaScript'
+console.log(string1.concat(string2))
+console.log(challange.repeat(2))
